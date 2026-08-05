3c419a1c2df0345b48b6eec3e19ec34c64c661d8
Sais <github@swgsais.com>
2026-08-05 00:07:49 -0400

    Retire NGE heroic jewelry primary stats

--- sku.0/sys.server/compiled/game/script/item/heroic_random_stat_item.java
+++ sku.0/sys.server/compiled/game/script/item/heroic_random_stat_item.java
@@ -8,17 +8,27 @@ public class heroic_random_stat_item extends script.base_script
     public heroic_random_stat_item()
     {
     }
-    public static final String[] STAT_ONE =
+    public static final String[] LEGACY_NGE_PRIMARY_MODIFIERS =
     {
         "agility_modified",
         "stamina_modified",
-        "constitution_modified"
-    };
-    public static final String[] STAT_TWO =
-    {
+        "constitution_modified",
         "precision_modified",
         "strength_modified",
         "luck_modified"
+    };
+    public static final String[] LEGACY_NGE_ACTION_MODIFIERS =
+    {
+        "expertise_action_weapon_0",
+        "expertise_action_weapon_1",
+        "expertise_action_weapon_2",
+        "expertise_action_weapon_4",
+        "expertise_action_weapon_5",
+        "expertise_action_weapon_6",
+        "expertise_action_weapon_7",
+        "expertise_action_weapon_9",
+        "expertise_action_weapon_10",
+        "expertise_action_weapon_11"
     };
     public static final String[] RANGED_SPEED_MODIFIERS =
     {
@@ -39,15 +49,11 @@ public class heroic_random_stat_item extends script.base_script
         "twohandlightsaber_speed",
         "polearmlightsaber_speed"
     };
-    public static final int[] STAT_VALS =
-    {
-        25,
-        25,
-        2
-    };
+    public static final int WEAPON_SPEED_VALUE = 2;
     public int OnAttach(obj_id self) throws InterruptedException
     {
-        if (!hasObjVar(self, "skillmod.bonus"))
+        removeLegacyNgeModifiers(self);
+        if (!hasWeaponSpeedModifier(self))
         {
             messageTo(self, "generateRandomStats", null, 3, false);
         }
@@ -55,7 +61,8 @@ public class heroic_random_stat_item extends script.base_script
     }
     public int OnInitialize(obj_id self) throws InterruptedException
     {
-        if (!hasObjVar(self, "skillmod.bonus"))
+        removeLegacyNgeModifiers(self);
+        if (!hasWeaponSpeedModifier(self))
         {
             messageTo(self, "generateRandomStats", null, 3, false);
         }
@@ -63,13 +70,45 @@ public class heroic_random_stat_item extends script.base_script
     }
     public int generateRandomStats(obj_id self, dictionary params) throws InterruptedException
     {
-        if (!hasObjVar(self, "skillmod.bonus"))
+        removeLegacyNgeModifiers(self);
+        if (!hasWeaponSpeedModifier(self))
         {
-            setObjVar(self, "skillmod.bonus." + STAT_ONE[rand(0, STAT_ONE.length - 1)], STAT_VALS[0]);
-            setObjVar(self, "skillmod.bonus." + STAT_TWO[rand(0, STAT_TWO.length - 1)], STAT_VALS[1]);
-            setObjVar(self, "skillmod.bonus." + getWeightedWeaponSpeedModifier(), STAT_VALS[2]);
+            setObjVar(self, "skillmod.bonus." + getWeightedWeaponSpeedModifier(), WEAPON_SPEED_VALUE);
         }
         return SCRIPT_CONTINUE;
+    }
+    public void removeLegacyNgeModifiers(obj_id self) throws InterruptedException
+    {
+        removeSkillModifiers(self, LEGACY_NGE_PRIMARY_MODIFIERS);
+        removeSkillModifiers(self, LEGACY_NGE_ACTION_MODIFIERS);
+    }
+    public void removeSkillModifiers(obj_id self, String[] modifiers) throws InterruptedException
+    {
+        for (String modifier : modifiers)
+        {
+            String objVar = "skillmod.bonus." + modifier;
+            if (hasObjVar(self, objVar))
+            {
+                removeObjVar(self, objVar);
+            }
+        }
+    }
+    public boolean hasWeaponSpeedModifier(obj_id self) throws InterruptedException
+    {
+        return hasAnySkillModifier(self, RANGED_SPEED_MODIFIERS) ||
+            hasAnySkillModifier(self, MELEE_SPEED_MODIFIERS) ||
+            hasAnySkillModifier(self, LIGHTSABER_SPEED_MODIFIERS);
+    }
+    public boolean hasAnySkillModifier(obj_id self, String[] modifiers) throws InterruptedException
+    {
+        for (String modifier : modifiers)
+        {
+            if (hasObjVar(self, "skillmod.bonus." + modifier))
+            {
+                return true;
+            }
+        }
+        return false;
     }
     public String getWeightedWeaponSpeedModifier() throws InterruptedException
     {
